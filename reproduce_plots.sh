2d1c07d838104163953e9c92b9789e72f1aef6a4
Kyle Hogan <klhogan@mit.edu>
2025-09-07 11:01:31 -0400

    help message for script

--- reproduce_plots.sh
+++ reproduce_plots.sh
@@ -2,6 +2,12 @@
 
 # Bash script to run distinguishing_game.py for specified plot types
 # Usage: ./reproduce_plots.sh [--clean] [--small] [--cores N] [plot_type1 plot_type2 ...]
+# Options:
+#   --clean         Remove previous data for each plot type before running
+#   --small         Use small number of trials for quick runs
+#   --cores N       Number of CPU cores to use (default: 8)
+#   plot_type       One or more plot types to run: targeting, engagement, epsilon, private_v_nonprivate (default: all)
+#   --help          Show this help message and exit
 
 SCRIPT="distinguishing_game.py"
 
@@ -9,6 +15,12 @@ CLEAN_FLAG=""
 SMALL_FLAG=""
 CORES=8
 PLOT_TYPES=()
+
+# Help message
+if [[ "$1" == "--help" ]]; then
+    grep '^#' "$0" | sed 's/^# \{0,1\}//'
+    exit 0
+fi
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
